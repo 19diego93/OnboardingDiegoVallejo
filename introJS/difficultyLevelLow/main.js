78c906a89d64c492c300ee17806dddf911b2f778
diego vallejo <vallejodiego@live.com>
2024-03-19 19:06:23 -0300

    low level introJS

---
+++ introJS/difficultyLevelLow/main.js
@@ -0,0 +1,9 @@
+const myName = "Diego";
+const myLastName = "Vallejo";
+const myAge = 31;
+const myPet = "Donna";
+const myPetAge = 6;
+const fullName = myName + " " + myLastName;
+const presentationText = `Hello my name is ${fullName} I´m ${myAge} years old, here with me is ${myPet} my cat, she is ${myPetAge} years old.`;
+
+console.log(presentationText);
