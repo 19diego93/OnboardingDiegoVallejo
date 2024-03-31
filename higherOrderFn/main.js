24396d89b84bf50725af1ee7bf35ae14a7d46616
diego vallejo <vallejodiego@live.com>
2024-03-31 19:27:22 -0300

    tailwind dom and higherOrderFn

---
+++ higherOrderFn/main.js
@@ -0,0 +1,132 @@
+//1
+/* function printMessage(message) {
+  console.log(message);
+} */
+// let printMessage = (message) => console.log(message);
+
+//2
+/* function createMultplication(number1, number2) {
+  let result = number1 * number2;
+  return result;
+} */
+// let createMultplication = (number1, number2) => number1 * number2;
+
+//3
+/* const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let newArray = array.map((numero) => createMultplication(numero*3));
+
+console.log(newArray);
+console.log(array) */
+
+//4 intenté utilizar filter() pero me encontre que no podia ver el lenght del array de vuelto ( me decia que era any)
+/* let topAbv;
+let filterTen = (array) => {
+  topAbv = array.sort((a, b) => b.abv - a.abv).splice(0, 10);
+};
+filterTen(beers, topAbv);
+console.log(topAbv);
+ */
+//5
+
+/* let lessBitter;
+let filterTen = (array) => {
+  lessBitter = array.sort((a, b) => a.ibu - b.ibu).splice(0, 10);
+};
+filterTen(beers);
+console.log(lessBitter);
+ */
+//6
+
+/* let beerFound;
+let beerFinder = (array, beerName) => {
+  beerFound = array.find((beer) => beer.name.toLowerCase() == beerName.toLowerCase());
+};
+beerFinder(beers,"Fake Lager");
+console.log(beerFound);
+ */
+//7
+/* 
+let ibuFound;
+let ibuFinder = (array, ibuNum) => {
+  ibuFound = array.find((beer) => beer.ibu == ibuNum);
+  if (!ibuFound) {
+    console.log("There is no beer with an ibu of (value entered).");
+  }
+};
+ibuFinder(beers, 50);
+console.log(ibuFound);
+ */
+//8 tuve problemas con las llaves en el callback
+
+/* let nameFound;
+let nameFinder = (beerName) => {
+  nameFound = beers.findIndex(
+    (beer) => beer.name.toLowerCase() == beerName.toLowerCase()
+  );
+  if (nameFound == -1) {
+    console.log(`${beerName} does not exist.`);
+  } else {
+    nameFound++;
+  }
+};
+nameFinder("Bramling X");
+console.log(nameFound); */
+
+//9
+/* let underTheLimit;
+let filterBeers = (array, limit) => {
+  underTheLimit = array
+    .filter((beer) => beer.abv < limit);
+    for (const beer of underTheLimit) {
+      beer.name = beer.name;
+      beer.alcohol = beer.abv;
+      beer.bitterness = beer.ibu;      
+    }
+};
+filterBeers(beers, 10);
+console.log(underTheLimit); */
+
+//10
+/* let filteredBeer;
+let beerFilter = (array, prop, boolean) => {
+  if (boolean) {
+    filteredBeer = array.toSorted((a, b) => a[prop] - b[prop]).splice(0, 10);
+  } else {
+    filteredBeer = array.toSorted((a, b) => b[prop] - a[prop]).splice(0, 10);
+  }
+  return filteredBeer;
+};
+beerFilter(beers, "abv", false);
+console.log(filteredBeer); */
+
+//11
+let contenedor = document.getElementById("contenedor");
+let template = document.createElement("template");
+
+let tableConstructor = (array) => {
+  let rows = array.map(
+    (beer) => ` <tr>
+<td>${beer.name}</td>
+<td>${beer.abv}</td>
+<td>${beer.ibu}</td>
+</tr>`
+  );
+  let table = `<table>
+  <thead>
+      <tr>
+          <th>Name</th>
+          <th>ABV</th>
+          <th>IBU</th>
+      </tr>
+      ${rows.join("")}
+  </thead>
+  <tbody>
+  
+  </tbody>
+</table>`;
+
+  template.innerHTML = table;
+  contenedor.innerHTML = template;
+};
+tableConstructor(beers);
+console.log([template]);
